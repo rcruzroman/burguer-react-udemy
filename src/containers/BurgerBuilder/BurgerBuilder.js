5c00b4adef30e474405f443c7242b0939915a52e
Rafa Cruz <rafaelcruzroman@gmail.com>
2018-07-21 12:41:21 +0200

    Added buttons to backdrop

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -80,6 +80,10 @@ class BurgerBuilder extends  Component {
         this.setState({purchasing: false});
     }
 
+    purchaseContinueHandler = () => {
+        alert('CONTINUE!!!');
+    }
+
 
     render () {
         const disabledInfo = {
@@ -91,7 +95,10 @@ class BurgerBuilder extends  Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCloseHandler}>
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCloseHandler}
+                        purchaseContinued={this.purchaseContinueHandler}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
