dbb153ac5e4ce2bb51f5c1558e5ff1b85d8eb521
Rafa Cruz <rafaelcruzroman@gmail.com>
2018-08-15 19:55:23 +0200

    Added responsive elements

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Aux>
-        <SideDrawer/>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
 
-export default layout;
+    state = {
+        showSideDrawer : true
+    }
+
+    sideDrawerCloseHandler = () => {
+        this.setState ({showSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <Aux>
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+                <Toolbar/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    };
+}
+
+export default Layout;
